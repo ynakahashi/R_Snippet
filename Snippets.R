5fa8999c0f51e471af8feb11d6ae67f1ec5ccad7
ynakahashi <ynakahashi1003@gmail.com>
2017-04-17 21:08:26 +0900

    Add time-series decomposition

--- Snippets.R
+++ Snippets.R
@@ -4,3 +4,28 @@ plot(as.zoo(as.ts(
    plot.type = "single",
    col=1:2)
 
+## Seasonal decomposition of time series panel data
+datTmpAll <- c()
+for(Car in model_name){
+   for(Reg in region_name){
+      for (Cust in customer) {
+
+         ## select target carmodel, region, variable
+         obj_name <- paste0("NUM_ORDERS_", Cust)
+         datSel   <- datOrd_Target %>%
+            filter(CAR.MODEL == Car, Region == Reg) %>%
+            select_(obj_name, "YEARWEEK") %>% # use "select_" instead of "select"
+            rename_("TARGET" = obj_name)      # use "rename_" instead of "rename"
+         
+         ## run stl
+         datSTL    <- stl(ts(datSel$TARGET, frequency = 52),
+                          s.window = "periodic")$time.series %>% data.frame()
+         
+         ## rbind
+         datTmpAll <- datSTL %>%
+            mutate("CarModel" = Car, "Region" = Reg, "Customer" = Cust) %>%
+            mutate("YEARWEEK" = datSel$YEARWEEK) %>%
+            bind_rows(datTmpAll)
+      }
+   }
+}
