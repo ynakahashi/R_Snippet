459464163922e34fa5cf70307121435647a48e8d
ynakahashi <ynakahashi1003@gmail.com>
2017-08-27 23:16:56 +0900

    no message

--- RandomForest_To_SQL.R
+++ RandomForest_To_SQL.R
@@ -1,4 +1,5 @@
 library(randomForest)
+set.seed(123)
 res <- randomForest(Species ~ ., data = iris, ntree = 2)
 
 # for (i in 1:(res$ntree)) {
@@ -45,7 +46,29 @@ Make_SQL <- function(Rule_Table, cnt, ind = 1) {
 
 for (i in 1:(res$ntree)) {
    Rule_Table <- getTree(res, k = i, labelVar = TRUE)
-   cat(paste0("INSERT INTO tbl_rf \nSELECT"))
+   cat(paste0("INSERT INTO tbl_rf \nSELECT\n   id, "))
    Make_SQL(Rule_Table, 1, 1)
    cat(paste0(" as tree", i, "\nFROM\n", "   input_data", ";\n\n"))
 }
+
+
+cat(paste0("INSERT INTO rf_predictions\n",
+           "SELECT\n   a.id,\n   a.pred\n",
+           "FROM\n   (\n      SELECT id as id, pred, COUNT(*) as cnt \n",
+           "      FROM tbl_rf\n      GROUP BY id, pred\n   ) a\n",
+           "   INNER JOIN\n   (\n      ",
+           "SELECT id, MAX(cnt) as cnt\n",
+           "      FROM\n",
+           "         (\n", 
+           "            SELECT id as id, pred, COUNT(*) as cnt\n", 
+           "            FROM tbl_rf\n",
+           "            GROUP BY id, pred\n", 
+           "         )\n",
+           "      GROUP BY id\n   ) b\n",
+           "      ON a.id = b.id AND a.cnt = b.cnt;\n\n"))
+
+
+
+
+
+
