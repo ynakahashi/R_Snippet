f6ca80c53b31bf6bca97467ee7a6855b68fb0bb3
ynakahashi <ynakahashi1003@gmail.com>
2019-10-17 13:49:14 +0900

    ソースを見ながらコメントをつけた

--- Source/gam/gam_gam_fit.r
+++ Source/gam/gam_gam_fit.r
@@ -1,24 +1,34 @@
 ### gam::gam.fit
+### gam.fit は x, y に加えて smooth.frame を受け取る。これは gam で作った mf で、中身は平滑化に関する情報を持った data.frame 
 function (x, y, smooth.frame, weights = rep(1, nobs), start = NULL, 
     etastart = NULL, mustart = NULL, offset = rep(0, nobs), family = gaussian(), 
     control = gam.control()) 
 {
+    ### 列名
     ynames <- if (is.matrix(y)) 
         dimnames(y)[[1]]
     else names(y)
     xnames <- dimnames(x)[[2]]
+
+    ### データの行数、列数
     nobs <- NROW(y)
     nvars <- ncol(x)
+
+    ## その他の gam.control オプション
     maxit <- control$maxit
     bf.maxit <- control$bf.maxit
     epsilon <- control$epsilon
     bf.epsilon <- control$bf.epsilon
     trace <- control$trace
+
+    ### digits, weigths, offset
     digits <- -log10(epsilon) + 1
     if (is.null(weights)) 
         weights <- rep.int(1, nobs)
     if (is.null(offset)) 
         offset <- rep.int(0, nobs)
+    
+    ### family に関するパラメータ
     variance <- family$variance
     dev.resids <- family$dev.resids
     aic <- family$aic
@@ -41,8 +51,11 @@ function (x, y, smooth.frame, weights = rep(1, nobs), start = NULL,
         eval(family$initialize)
         mustart <- mukeep
     }
+    ### eta の初期値
     eta <- if (!is.null(etastart)) 
         etastart
+    
+    ### エラーチェック
     else if (!is.null(start)) 
         if (length(start) != nvars) 
             stop("Length of start should equal ", nvars, " and correspond to initial coefs for ", 
@@ -54,10 +67,17 @@ function (x, y, smooth.frame, weights = rep(1, nobs), start = NULL,
             else x %*% start)
         }
     else family$linkfun(mustart)
+
+    ### mu の初期値
     mu <- linkinv(eta)
     if (!(validmu(mu) && valideta(eta))) 
         stop("Can't find valid starting values: please specify some")
+
+    ### デビアンス（残差平方和）
     new.dev <- sum(dev.resids(y, mu, weights))
+
+    ### smoothers が指定されている場合に smoother を抽出する
+    ### 今回のケースでは s が入る
     a <- attributes(attr(smooth.frame, "terms"))
     smoothers <- a$specials
     if (length(smoothers) > 0) {
@@ -70,14 +90,21 @@ function (x, y, smooth.frame, weights = rep(1, nobs), start = NULL,
             else NULL
         }
     }
+
+    ### smoother が指定されている場合、ここが処理される
     if (length(smoothers) > 0) {
         gam.wlist = gam.smoothers()$wlist
         smooth.labels <- a$term.labels[unlist(smoothers)]
         assignx <- attr(x, "assign")
         assignx <- assign.list(assignx, a$term.labels)
         which <- assignx[smooth.labels]
+
+        ### ２つ以上の smoothers が指定されている場合は backfit として general.wam が指定される
+        ### wam は weighted additive model
         if (length(smoothers) > 1) 
             bf <- "general.wam"
+
+        ### 今回のケース（平滑化の関数として s を指定）はこちらで、 s.wam が指定される。
         else {
             sbf <- match(names(smoothers), gam.wlist, FALSE)
             bf <- if (sbf) 
@@ -90,11 +117,14 @@ function (x, y, smooth.frame, weights = rep(1, nobs), start = NULL,
         dimnames(s) <- list(names(y), names(which))
         fit <- list(smooth = s, smooth.frame = smooth.frame)
     }
+
+    ### backfit 向けの call 生成
     else {
         bf.call <- expression(lm.wfit(x, z, wz, method = "qr", 
             singular.ok = TRUE))
         bf <- "lm.wfit"
     }
+
     old.dev <- 10 * new.dev + 10
     for (iter in 1:maxit) {
         good <- weights > 0
@@ -107,28 +137,47 @@ function (x, y, smooth.frame, weights = rep(1, nobs), start = NULL,
         if (any(is.na(mu.eta.val[good]))) 
             stop("NAs in d(mu)/d(eta)")
         good <- (weights > 0) & (mu.eta.val != 0)
+
+        ### z を生成。ただし bf.call 、すなわち lm.wfit で二番目の引数は y なので、この z は目的変数の意味
         z <- eta - offset
         z[good] <- z[good] + (y - mu)[good]/mu.eta.val[good]
+
+        ### wz を生成。 lm.wfit における重み。今回のケースでは無視して良い
         wz <- weights
         wz[!good] <- 0
         wz[good] <- wz[good] * mu.eta.val[good]^2/varmu[good]
+
+        ### ここで lm.wfit が呼ばれ、 backfit が行われる
         fit <- eval(bf.call)
+
+        ### オフセットを加算する
         eta <- fit$fitted.values + offset
+
+        ### eta から mu に持ってくる
         mu <- linkinv(eta)
+
+        ### デビアンスを更新
         old.dev <- new.dev
         new.dev <- sum(dev.resids(y, mu, weights))
+
+        ### ここの trace は対角和ではなく、 gam のオプションでループごとのデビアンスをモニターできる
         if (trace) 
             cat("GAM ", bf, " loop ", iter, ": deviance = ", 
                 format(round(new.dev, digits)), " \n", sep = "")
+
+        ### ループの打ち切り判定
+        ### デビアンスが NA となった場合、警告を出して打ち切る
         if (is.na(new.dev)) {
             one.more <- FALSE
             warning("iterations terminated prematurely because of singularities")
         }
+        ### 差が十分に小さければ終了
         else one.more <- abs(old.dev - new.dev)/(old.dev + 0.1) > 
             epsilon
         if (!one.more) 
             break
     }
+
     fitqr <- fit$qr
     xxnames <- xnames[fitqr$pivot]
     nr <- min(sum(good), nvars)
@@ -143,6 +192,8 @@ function (x, y, smooth.frame, weights = rep(1, nobs), start = NULL,
     names(fit$residuals) <- ynames
     names(mu) <- ynames
     names(eta) <- ynames
+
+    ### eta and mu
     fit$additive.predictors <- eta
     fit$fitted.values <- mu
     names(fit$weights) <- ynames
